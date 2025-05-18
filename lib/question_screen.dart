ed9e2fbcd17fc2ff452451c8dc3b87259a239e96
ChetanVarunKanipakam <chetanvarun131@gmail.com>
2025-05-18 16:05:57 +0530

    added google fonts for flutter

--- lib/question_screen.dart
+++ lib/question_screen.dart
@@ -2,10 +2,10 @@ import 'package:flutter/material.dart';
 
 import 'package:basicquizapp/answer_button.dart';
 import 'package:basicquizapp/data/questions.dart';
+import 'package:google_fonts/google_fonts.dart';
 
-class QuestionScreen extends StatefulWidget{
+class QuestionScreen extends StatefulWidget {
   const QuestionScreen({super.key, required this.onselectedAnswer});
-
 
   final void Function(String) onselectedAnswer;
   @override
@@ -14,45 +14,56 @@ class QuestionScreen extends StatefulWidget{
   }
 }
 
-class _QuestionScreenState extends State<QuestionScreen>{
-  var currentQuestionState=0;
+class _QuestionScreenState extends State<QuestionScreen> {
+  var currentQuestionState = 0;
 
-
-  void answerQuestion(String answer){
+  void answerQuestion(String answer) {
     setState(() {
       widget.onselectedAnswer(answer);
       currentQuestionState++;
     });
   }
 
-
   @override
   Widget build(BuildContext context) {
-    var currentQuestion=questions[currentQuestionState];
+    var currentQuestion = questions[currentQuestionState];
     return SizedBox(
-      width:double.infinity,
+      width: double.infinity,
       child: Container(
         margin: EdgeInsets.all(40),
-      child: Column(
-        crossAxisAlignment: CrossAxisAlignment.stretch,
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-        Text(currentQuestion.question ,style: TextStyle(color: Colors.white),textAlign: TextAlign.center,),
-        const SizedBox(height: 30),
-        ...currentQuestion.questionShuffle(currentQuestion.answers).map((answer){
-          return Column(
-            crossAxisAlignment: CrossAxisAlignment.stretch,
-          children:[ AnswerButton(
-          answerText: answer,
-          onTap:(){
-            answerQuestion(answer);
-          },
+        child: Column(
+          crossAxisAlignment: CrossAxisAlignment.stretch,
+          mainAxisAlignment: MainAxisAlignment.center,
+          children: [
+            Text(
+              currentQuestion.question,
+              style: GoogleFonts.lato(
+                color: const Color.fromARGB(255, 220, 171, 253),
+                fontSize: 24,
+                fontWeight: FontWeight.bold
+                ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(height: 30),
+            ...currentQuestion.questionShuffle(currentQuestion.answers).map((
+              answer,
+            ) {
+              return Column(
+                crossAxisAlignment: CrossAxisAlignment.stretch,
+                children: [
+                  AnswerButton(
+                    answerText: answer,
+                    onTap: () {
+                      answerQuestion(answer);
+                    },
+                  ),
+                  SizedBox(height: 20),
+                ],
+              );
+            }),
+          ],
         ),
-        SizedBox(height: 20)]
-        );
-        })
-      ],)
-    )
+      ),
     );
   }
 }
